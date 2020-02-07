e485d243ec4bfebfcd93e511819e028f16086462
Andrey <tom.my2@i.ua>
2020-02-07 19:12:41 +0200

    DB

--- src/components/suggestForm/SuggestForm.vue
+++ src/components/suggestForm/SuggestForm.vue
@@ -10,6 +10,8 @@
       >Редактировать форму</b-form-checkbox>
     </header>
 
+    <div @click="dbtest">Fetch</div>
+
     <admin-blocks v-if="mode=='admin'"/>
     <publ-blocks v-if="mode=='publ'"/>
   </div>
@@ -18,6 +20,7 @@
 
 
 <script>
+import fetch from 'isomorphic-unfetch'
 import { mapActions, mapGetters } from "vuex";
 import AdminBlocks from "./admin/blocks/AdminBlocks.vue";
 import PublBlocks from "./publ/blocks/PublBlocks.vue";
@@ -41,6 +44,20 @@ export default {
   },
   mounted() {
     this.fetchBlocks();
+  },
+  methods: {
+    dbtest: async ({ req, query }) => {
+      const protocol = req
+        ? `${req.headers['x-forwarded-proto']}:`
+        : location.protocol
+      const host = req ? req.headers['x-forwarded-host'] : location.host
+      const pageRequest = `${protocol}//${host}/api/profiles?page=${query.page ||
+        1}&limit=${query.limit || 9}`
+      const res = await fetch(pageRequest)
+      const json = await res.json();
+      console.log(json);
+      return json;
+    }
   }
 };
 </script>
